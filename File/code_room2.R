5a541a078225c6562fff63563cf67b302828716e
Mifuyu KIRA <m.kira0331@gmail.com>
2023-08-12 15:06:17 +0900

    Select data

--- File/code_room2.R
+++ File/code_room2.R
@@ -1,1 +1,10 @@
-data <- read.csv("owid-co2-data.csv")
+data_raw <- read.csv("Data/owid-co2-data.csv")
+
+data <- data_raw |> 
+  dplyr::select(
+    country,
+    year,
+    gdp,
+    population,
+    co2_per_capita
+  )
